5548c97f6e22f7249f6095d2c06a8ad66912fc9c
pranjal <pranjalrana474@gmail.com>
2025-07-31 14:54:42 +0530

    mongodb project

---
+++ PROJECTS/PROJECT3/project3.py
@@ -0,0 +1,62 @@
+#########  MONGODB WITH PYTHON  ##################
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient("mongodb+srv://youtubepy:youtubepy@cluster0.wl4fpyu.mongodb.net/ytmanager?retryWrites=true&w=majority&appName=Cluster0")
+
+db = client["ytmanager"]
+video_collection = db["videos"]
+
+print(video_collection)
+
+def add_video(name, time):
+    video_collection.insert_one({"name":name, "time":time})
+
+def list_videos():
+    for video in video_collection.find():
+        print(f"ID: {video['_id']}, Name: {video['name']} and Time: {video['time']}")
+
+def update_video(video_id, new_name, new_time):
+    video_collection.update_one(
+        {'_id': ObjectId(video_id)},
+        {"$set": {"name": new_name, "time":new_time}}
+    )
+
+def delete_video(video_id):
+    video_collection.delete_one({"_id": ObjectId(video_id)})
+
+def main():
+    while True:
+        print("\n Youtube manager app")
+        print("1. List all videos")
+        print("2. Add new video")
+        print("3. Update a video")
+        print("4. Delete a video")
+        print("5. Exit the app")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+
+        elif choice == '3':
+            video_id = input("Enter the video id: ")
+            name = input("Enter the new video name: ")
+            time = input("Enter the new video time: ")
+            update_video(video_id, name, time)
+
+        elif choice == '4':
+            video_id = input("Enter the video id: ")
+            delete_video(video_id)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice")
+
+
+if __name__ == "__main__":
+    main()
